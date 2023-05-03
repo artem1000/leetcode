7fc65d8f01c8ab115d63fb6bb620a6786858b374
Artem Grinchenko <artem1000@gmail.com>
2023-05-03 17:38:29 -0400

    Added problem descriptions

--- arrayProduct/Main.java
+++ arrayProduct/Main.java
@@ -1,6 +1,17 @@
 package leetcode.arrayProduct;
 
 public class Main {
+
+    /*
+    * There is a function signFunc(x) that returns:
+
+        1 if x is positive.
+        -1 if x is negative.
+        0 if x is equal to 0.
+        You are given an integer array nums. Let product be the product of all values in the array nums.
+
+        Return signFunc(product).*/
+
     public static int arraySign(int[] nums) {
         int res = 1;
         for (int num : nums) {
